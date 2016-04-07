3444568e13817ef1e49deeab06f1035b93f992d4
lf.divino <lf.divino@gmail.com>
2016-04-07 00:12:29 -0300

    Modificado static files dos settings

--- projetotcc/settings.py
+++ projetotcc/settings.py
@@ -116,11 +116,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+#
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 
 
 # Crispy Forms
