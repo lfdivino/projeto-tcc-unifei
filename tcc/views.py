1d28d77e9a7bec07558f2d30f22f333943b8a4a6
lf.divino <lf.divino@gmail.com>
2016-04-14 16:01:13 -0300

    Alteracoes de css e adicionada funcao para ativacao de registro por email.

--- tcc/views.py
+++ tcc/views.py
@@ -160,8 +160,14 @@ def register_user(request):
 
             username = form.cleaned_data['username']
             email = form.cleaned_data['email']
-            salt = hashlib.sha1(str(random.random())).hexdigest()[:5]
-            activation_key = hashlib.sha1(salt+email).hexdigest()
+            random_string = str(random.random()).encode('utf8')
+
+            salt = hashlib.sha1(random_string).hexdigest()[:5]
+
+            salted = (salt + email).encode('utf8')
+
+            activation_key = hashlib.sha1(salted).hexdigest()
+
             key_expires = datetime.datetime.today() + datetime.timedelta(2)
 
             #Get user by username
@@ -173,14 +179,14 @@ def register_user(request):
             new_profile.save()
 
             # Send email with activation key
-            email_subject = 'Account confirmation'
-            email_body = "Hey %s, thanks for signing up. To activate your account, click this link within \
-            48hours http://127.0.0.1:8000/accounts/confirm/%s" % (username, activation_key)
+            email_subject = 'Siseel: Ativação de registro'
+            email_body = "Olá %s, obrigado por se registrar. Para ativar a sua conta, clique no link a seguir em até \
+            48hours http://127.0.0.1:8000/confirm/%s" % (username, activation_key)
 
-            send_mail(email_subject, email_body, 'myemail@example.com',
+            send_mail(email_subject, email_body, 'lf.divino@yahoo.com',
                 [email], fail_silently=False)
 
-            return HttpResponseRedirect('register_success')
+            return HttpResponseRedirect('/register_success')
     else:
         args['form'] = RegistrationForm()
 
@@ -197,19 +203,18 @@ def register_confirm(request, activation_key):
 
     #check if the activation key has expired, if it hase then render confirm_expired.html
     if user_profile.key_expires < timezone.now():
-        return render_to_response('user_profile/confirm_expired.html')
+        return render_to_response('confirm_expired.html')
     #if the key hasn't expired save user and set him as active and render some template to confirm activation
     user = user_profile.user
     user.is_active = True
     user.save()
-    return render_to_response('user_profile/confirm.html')
+    return render_to_response('login.html')
 
 
 def register_success(request):
     context = {
         'title': 'Obrigado por registrar!'
     }
-
 
     return render(request, "register_success.html", context)
 
