29d016d4bbdb2807c71700feb971d7361e45017b
lf.divino <lf.divino@gmail.com>
2016-04-22 13:29:16 -0300

    Mundacas no caminho do bd

--- projetotcc/settings.py
+++ projetotcc/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'projetotcc.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'db.sqlite3',
     }
 }
 
