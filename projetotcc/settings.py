fcf9918925c53f4e2b4a460a6015109669f639d5
lf.divino <lf.divino@gmail.com>
2016-04-22 13:22:57 -0300

    Configurando base de dados para o deploy no heroku

--- projetotcc/settings.py
+++ projetotcc/settings.py
@@ -95,10 +95,13 @@ WSGI_APPLICATION = 'projetotcc.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
