6731f8e91e11acb9cb6d0b6cec073060b4cfcff6
lf.divino <lf.divino@gmail.com>
2016-04-25 00:52:08 -0300

    v03: Static files

--- projetotcc/settings.py
+++ projetotcc/settings.py
@@ -67,7 +67,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
 ROOT_URLCONF = 'projetotcc.urls'
