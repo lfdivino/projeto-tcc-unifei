f45b6c080bc6cb90ce3936eae6b5497fef02e356
lf.divino <lf.divino@gmail.com>
2016-04-25 22:22:01 -0300

    v08: Alteracoes no template

--- static/js/funcoes.js
+++ static/js/funcoes.js
@@ -1,3 +1,13 @@
+$("#nav").addClass("js").before('<div id="menu">&#9776;</div>');
+$("#menu").click(function(){
+    $("#nav").toggle();
+});
+$(window).resize(function(){
+    if(window.innerWidth > 768) {
+        $("#nav").removeAttr("style");
+    }
+});
+
 $(".btn-respostas").click(function(){
     var col = $(this).parent();
     var row = col.parent();
